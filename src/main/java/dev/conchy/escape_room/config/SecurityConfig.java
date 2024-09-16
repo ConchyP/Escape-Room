eb21d460797c7d7000b557799601d23e92dd34c9
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-09-16 18:04:33 +0200

    create aplication properties for h2, mysql and configuration api end point

---
+++ src/main/java/dev/conchy/escape_room/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package dev.conchy.escape_room.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+}
