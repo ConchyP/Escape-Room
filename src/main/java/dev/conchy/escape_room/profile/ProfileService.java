1415d8d0d3a9d9f3704dc8aebfb1e22eec713590
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-09-17 20:57:51 +0200

    added profile service

---
+++ src/main/java/dev/conchy/escape_room/profile/ProfileService.java
@@ -0,0 +1,30 @@
+package dev.conchy.escape_room.profile;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileService(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public Profile save(Profile profile) {
+        return profileRepository.save(profile);
+    }
+
+    public String getEmailByUserId(Long userId) {
+        Optional<Profile> profileOptional = profileRepository.findById(userId);
+        if (profileOptional.isPresent()) {
+            return profileOptional.get().getEmail();
+        }
+
+        return null;
+    }
+        
+
+}
