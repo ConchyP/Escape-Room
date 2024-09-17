e182c19f8249b8cbe83bac84f70d6f14f1d52079
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-09-17 20:56:03 +0200

    addded profile repository

---
+++ src/main/java/dev/conchy/escape_room/profile/ProfileRepository.java
@@ -0,0 +1,11 @@
+package dev.conchy.escape_room.profile;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByUserId(Long userId);
+}
