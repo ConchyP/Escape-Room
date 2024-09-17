5a42e616b68ac72ff89c4a84d0dbda4e9eb95ca9
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-09-17 20:50:16 +0200

    added user dto

---
+++ src/main/java/dev/conchy/escape_room/users/UserDto.java
@@ -0,0 +1,22 @@
+package dev.conchy.escape_room.users;
+
+
+public class UserDto { 
+
+    private String username;
+    private String password;
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public UserDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
