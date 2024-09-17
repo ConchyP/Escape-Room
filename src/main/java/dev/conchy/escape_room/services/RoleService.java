e8e9ab94da9a1f24fb073c672654331ee5a95aa4
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-09-17 19:35:37 +0200

    added role service

--- src/main/java/dev/conchy/escape_room/services/RoleService.java
+++ src/main/java/dev/conchy/escape_room/services/RoleService.java
@@ -1,9 +1,8 @@
 package dev.conchy.escape_room.services;
 
-import javax.management.relation.RoleNotFoundException;
-
 import org.springframework.stereotype.Service;
 
+import dev.conchy.escape_room.exceptions.RoleException;
 import dev.conchy.escape_room.models.Role;
 import dev.conchy.escape_room.repositories.RoleRepository;
 
@@ -16,7 +15,7 @@ public class RoleService {
 }
 
 public Role getById(Long id) {
-        Role role = repository.findById(id).orElseThrow(() -> new RoleNotFoundException("Role not found"));
+        Role role = repository.findById(id).orElseThrow(() -> new RoleException("Role not found"));
         return role;
     }
 
