fc3eadc73a8043efa397a260d29370d42c426336
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-09-26 01:25:27 +0200

    Added escape room service

---
+++ src/main/java/dev/conchy/escape_room/escapeRooms/services/EscapeRoomService.java
@@ -0,0 +1,45 @@
+package dev.conchy.escape_room.escaperooms.services;
+
+import java.util.List;
+
+import dev.conchy.escape_room.escaperooms.exceptions.EscapeRoomsNotFoundException;
+
+import dev.conchy.escape_room.escaperooms.repositories.EscapeRoomRepository;
+import dev.conchy.escape_room.escaperooms.models.EscapeRoom;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EscapeRoomService {
+
+    @Autowired
+    private EscapeRoomRepository repository;
+
+    public List<EscapeRoom> getAllEscapeRooms() {
+        return repository.findAll();
+    }
+
+    public EscapeRoom getEscapeRoomById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new EscapeRoomsNotFoundException("Escape Room not found"));
+    }
+
+    public EscapeRoom createEscapeRoom(EscapeRoom escapeRoom) {
+        return repository.save(escapeRoom);
+    }
+
+    public EscapeRoom updateEscapeRoom(EscapeRoom escapeRoom) {
+        if (!repository.existsById(escapeRoom.getId())) {
+            throw new EscapeRoomsNotFoundException("Escape Room not found");
+        }
+        return repository.save(escapeRoom);
+    }
+
+    public void deleteEscapeRoomById(Long id) {
+        if (!repository.existsById(id)) {
+            throw new EscapeRoomsNotFoundException("Escape Room not found");
+        }
+        repository.deleteById(id);
+    }
+
+}
