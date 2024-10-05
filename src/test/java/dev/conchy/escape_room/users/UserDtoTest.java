950ec22df4862c8f9233bd4c4d7607e823c8558e
Conchy Pereira <169025186+ConchyP@users.noreply.github.com>
2024-10-05 18:51:17 +0200

    Done test for user dto

--- src/test/java/dev/conchy/escape_room/users/UserDtoTest.java
+++ src/test/java/dev/conchy/escape_room/users/UserDtoTest.java
@@ -1,3 +1,24 @@
 package dev.conchy.escape_room.users;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class UserDtoTest {
+
+    @Test
+    public void testUserDtoConstructorAndGetters() {
+      
+        String expectedUsername = "testUser";
+        String expectedPassword = "testPassword";
+
+   
+        UserDto userDto = new UserDto(expectedUsername, expectedPassword);
+
+        
+        assertEquals(expectedUsername, userDto.getUsername(), "El username debe ser igual al valor esperado.");
+
+        assertEquals(expectedPassword, userDto.getPassword(), "El password debe ser igual al valor esperado.");
+    }
+}
 
